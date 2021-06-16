70f178b6401e989baaa1b5bbb0160b10d0d4b484
Nandakishor <nandakishornk01@gmail.com>
2021-06-17 00:26:17 +0530

    initial

--- solns.c
+++ solns.c
@@ -1,2 +1,46 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+
+int max(int a[],int n)
+{
+	int i,m=-2048;
+	for(i=0;i<n;i++)
+		if(a[i]>m)
+			m=a[i];
+	return m;
+}
+
+int min(int a[],int n)
+{
+	int i,m=2048;
+	for(i=0;i<n;i++)
+		if(a[i]<m)
+			m=a[i];
+	return m;
+}
+
+float average(int a[], int n)
+{
+	int s=0,i;
+	for(i=0;i<n;i++)
+		s+=a[i];
+	return float(s)/n;
+}
+
+int mode(int a[],int n)
+{
+	int i,l,h,c[100],m=-2048,mode;
+	l=min(a,n);
+	h=max(a,n);
+	for(i=l;i<(h+1);i++)
+		c[i]==0;
+	for(i=0;i<n;i++)
+		c[a[i]]++;
+	for(i=l;i<(h+1);i++)
+		if(c[i]>m)
+		{
+			m=a[i];
+			mode=i;
+		}
+	return mode;
+}
