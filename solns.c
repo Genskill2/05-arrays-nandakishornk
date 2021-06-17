a2c6ececb6d484b999d28b7378d0f3c8a783b53e
nandakishornk <84626570+nandakishornk@users.noreply.github.com>
2021-06-17 20:46:21 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -22,29 +22,24 @@ int min(int a[],int n)
 float average(int a[], int n)
 {
 	int s=0,i;
-	float av;
 	for(i=0;i<n;i++)
 		s+=a[i];
-	av=s/n;
-	return av;
+	return s/n;
 }
 
 int mode(int a[],int n)
 {
-	int i,h=-2048,c[100],m=-2048,mode;
+	int i,j,c,oc=0,mode;
 	for(i=0;i<n;i++)
-		if(a[i]>h)
-			h=a[i];
-	for(i=0;i<=h;i++)
-		c[i]==0;
-	for(i=0;i<n;i++)
-		c[a[i]]++;
-	for(i=0;i<=h;i++)
 	{
-		if(c[i]>m)
+		c=0;
+		for(j=0;j<n;j++)
+			if(a[j]==a[i])
+				c++;
+		if(c>oc)
 		{
-			m=a[i];
-			mode=i;
+			oc=c;
+			mode=a[i];
 		}
 	}
 	return mode;
